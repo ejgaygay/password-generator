295a3946cbe65a2067d2b85e9167d02836c40421
EJ Gaygay <74251098+ejgaygay@users.noreply.github.com>
2021-12-14 03:06:52 -0500

    Add files via upload

---
+++ ASCIIValues.java
@@ -0,0 +1,57 @@
+public class ASCIIValues {
+    private int min;
+    private int max;
+
+    public ASCIIValues(int min, int max){
+        this.min = min;
+        this.max = max;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+
+    public int getMin(){
+        return min;
+    }
+
+    public void setMax(int max){
+        this.max = max;
+    }
+
+    public int getMax(){
+        return max;
+    }
+/*
+    public static void main(String [] args) {
+        //random testing
+        System.out.println((int)(Math.random()*4));
+        System.out.println((int)(Math.random()*4));
+        System.out.println((int)(Math.random()*4));
+        System.out.println((int)(Math.random()*4));
+        System.out.println((int)(Math.random()*4));
+        System.out.println((int)(Math.random()*4));
+        System.out.println((int)(Math.random()*4));
+        System.out.println((int)(Math.random()*4));
+        System.out.println((int)(Math.random()*4));
+        System.out.println((int)(Math.random()*4));
+        System.out.println((int)(Math.random()*4));
+        System.out.println((int)(Math.random()*4));
+        System.out.println((int)(Math.random()*4));
+        System.out.println((int)(Math.random()*4));
+        System.out.println((int)(Math.random()*4));
+        System.out.println((int)(Math.random()*4));
+        System.out.println((int)(Math.random()*4));
+        System.out.println((int)(Math.random()*4));
+        System.out.println((int)(Math.random()*4));
+        System.out.println((int)(Math.random()*4));
+        System.out.println((int)(Math.random()*4));
+        System.out.println((int)(Math.random()*4));
+        System.out.println((int)(Math.random()*4));
+        System.out.println((int)(Math.random()*4));
+        System.out.println((int)(Math.random()*4));
+        System.out.println((int)(Math.random()*4));
+
+    }
+    */
+}
---
+++ PasswordGenerator.java
@@ -0,0 +1,126 @@
+import java.awt.*;
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.util.*;
+import java.math.*;
+import javax.swing.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+//bug found line 28 beyond - fix it then reorganize code base to be more readable, do the javadocs, finally fix the ugly layout to look better
+
+/**
+ * A password generator that generates a random password and can copy to the clipboard.
+ */
+class PassWordGenerator {
+
+    public static String generatePassword(boolean lowercase, boolean uppercase, boolean numbers, boolean symbols, int length){
+        //33-47 symbols
+        //48-57 numbers
+        //65-90 uppercase
+        //97-122 lowercase
+        StringBuilder password = new StringBuilder();
+        //procedure
+        //randomly choose between lower, upper, number, or symbol
+        //bug - find way to check lower and numbers or maybe lower symbols, etc.
+        ASCIIValues [] choices = new ASCIIValues[4];
+        int nums = 0;
+        boolean check = false;
+        if (lowercase){
+            choices[nums++] = new ASCIIValues(97,122);
+            check = true;
+        }
+        if (uppercase){
+            choices[nums++] = new ASCIIValues(65,90);
+            check = true;
+        }
+        if (numbers){
+            choices[nums++] = new ASCIIValues(48,57);
+            check = true;
+        }
+        if (symbols){
+            choices[nums++] = new ASCIIValues(33,47);
+            check = true;
+        }
+
+        if(!check) {
+            return "Please check a parameter.";
+        }
+
+        for(int i = 0; i < length; i++){
+            ASCIIValues range = choices[((int)(Math.random()*(nums)))];
+            String letter = Character.toString((int) (Math.random()*(range.getMax()-range.getMin()) + range.getMin()));
+            password.append(letter);
+        }
+        return password.toString();
+    }
+
+    public static void writeToClipboard(String s) {
+        StringSelection passwordCpy = new StringSelection(s);
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(passwordCpy,null);
+    }
+
+    //work in progress or maybe not at all.
+    public void revealPassword(){
+
+    }
+    public static void main(String [] args){
+        //init the frame
+        JFrame mainFrame = new JFrame("Password Generator 5000");
+        mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        //mainFrame.setSize(new Dimension(350, 350));
+        mainFrame.setLayout(new FlowLayout());
+
+        //password options
+        Checkbox lowercase = new Checkbox("lowercase", true);
+        Checkbox uppercase = new Checkbox("UPPERCASE", true);
+        Checkbox numbers = new Checkbox("0-9", false);
+        Checkbox symbols = new Checkbox("?/!*", false);
+
+        //length of password, uses a slider
+        JSlider passLength = new JSlider(JSlider.HORIZONTAL, 1, 20, 9);
+        passLength.setMajorTickSpacing(5);
+        passLength.setMinorTickSpacing(1);
+        passLength.setPaintTicks(true);
+        passLength.setPaintLabels(true);
+
+        //reveal or unreveal password
+        
+
+        //text field where password will be generated
+        TextComponent passwordField = new TextField("Password generated here...");
+        passwordField.setEditable(false);
+
+        //button action
+        Button generateButton = new Button("Generate");
+        generateButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                passwordField.setText(generatePassword(lowercase.getState(), uppercase.getState(), numbers.getState(), symbols.getState(), passLength.getValue()));
+            }
+        });
+
+        //do this when creating a button
+        Button copyButton = new Button("Copy");
+        copyButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                PassWordGenerator.writeToClipboard(passwordField.getText());
+            } 
+        });
+
+        //adding all of our components onto the main frame.
+        mainFrame.add(lowercase);
+        mainFrame.add(uppercase);
+        mainFrame.add(numbers);
+        mainFrame.add(symbols);
+        mainFrame.add(passwordField);
+        mainFrame.add(passLength);
+        mainFrame.add(generateButton);
+        mainFrame.add(copyButton);
+        //keep it last since it turns on the frame
+        mainFrame.pack();
+        mainFrame.setVisible(true);
+    }
+}
